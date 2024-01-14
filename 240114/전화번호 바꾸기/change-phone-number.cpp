31d6753d6ebd88d783b3b9937793b903f0d3932c
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-14 18:41:46 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240114/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+    cout << "0" << a << "-" << c << "-" << b;
+    return 0;
+}
