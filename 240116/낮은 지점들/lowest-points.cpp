92cea77485da0139e1a11a56cdcb1075d1dfac04
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-16 08:41:09 +0900

    Title: 낮은 지점들, Time: 74ms, Memory: 1MB, Status: Wrong Answer - CodeTree

---
+++ 240116/낮은 지점들/lowest-points.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+int main() {
+    unordered_map<int, int> hashmap;
+    int n, x, y, sum = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        if(hashmap.find(x) != hashmap.end())
+        {
+            if(y < hashmap[x])
+            {
+                hashmap[x] = y;
+            }
+        }
+        else
+        {
+            hashmap[x] = y;
+        }
+    }
+
+    for(auto h : hashmap)
+    {
+        sum += h.second;
+    }
+
+    cout << sum;
+    return 0;
+}
