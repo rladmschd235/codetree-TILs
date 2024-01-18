38b6dc55b692d7a27a58da84a919b6f1fb6b5e6f
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-18 11:41:52 +0900

    Title: 최솟값 3개, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240118/최솟값 3개/top-3-smallest-number.cpp
+++ 240118/최솟값 3개/top-3-smallest-number.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <queue>
-#include <vector>
 using namespace std;
 
 int main() {
     priority_queue<int> pq;
-    vector<int> arr;
     int n, m;
     long long int result = 1;
 
@@ -14,15 +12,10 @@ int main() {
     for (int i = 0; i < n; i++)
     {
         cin >> m;
-        arr.push_back(m);
-
+        pq.push(-m);
         if (arr.size() > 2)
         {
             result = 1;
-            for (int j = 0; j < arr.size(); j++)
-            {
-                pq.push(-arr[j]);
-            }
             for (int j = 0; j < 3; j++)
             {
                 result *= -pq.top();
