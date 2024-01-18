d3e77a976caeca49226e3e3c8a92fc35e5c5e0a8
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-18 11:34:32 +0900

    Title: 최솟값 3개, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240118/최솟값 3개/top-3-smallest-number.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int main() {
+    priority_queue<int> pq;
+    vector<int> arr;
+    int n, m, result = 1;
+
+    cin >> n;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> m;
+        arr.push_back(m);
+
+        if (i > 1)
+        {
+            result = 1;
+            for (int j = 0; j < arr.size(); j++)
+            {
+                pq.push(-arr[j]);
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                result *= -pq.top();
+                pq.pop();
+            }
+            cout << result << "\n";
+        }
+        else
+        {
+            cout << -1 << "\n";
+        }
+    }
+    return 0;
+}
