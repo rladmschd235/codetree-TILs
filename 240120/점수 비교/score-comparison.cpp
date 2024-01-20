411994dc0a2fa8f38fec8190002c4eef9b41ddf0
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-20 20:49:03 +0900

    Title: 점수 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240120/점수 비교/score-comparison.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A1, B1, A2, B2;
+    cin >> A1 >> A2 >> B1 >> B2;
+    if(A1 > B1 && A2 > B2)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 1;
+    }
+    return 0;
+}
