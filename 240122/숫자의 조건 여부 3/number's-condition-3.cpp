fd4f51b2ad9d8b5c8c23f479903bd18d076cafd5
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-22 23:38:36 +0900

    Title: 숫자의 조건 여부 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240122/숫자의 조건 여부 3/number's-condition-3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    if(a%3 == 0 || a%19 == 0)
+    {
+        cout << "True";
+    }
+    else
+    {
+        cout << "False";
+    }
+    return 0;
+}
