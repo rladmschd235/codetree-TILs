c2a86a3fc09b29c74a175018ab464412d374dc8a
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-23 18:14:30 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if(n == 2)
+    {
+        cout << 28;
+    }
+    else if(n%2 == 1)
+    {
+        cout << 31;
+    }
+    else if(n%2 == 0)
+    {
+        cout << 30;
+    }
+
+    return 0;
+}
