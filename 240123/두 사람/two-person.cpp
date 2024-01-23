d3449e8e22124fc8abe145d67a19a9ad2c2bbae7
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-23 17:49:52 +0900

    Title: 두 사람, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240123/두 사람/two-person.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A_age, B_age;
+    char A_sex, B_sex;
+
+    cin >> A_age >> A_sex >> B_age >> B_sex;
+
+    if( (A_age >= 19 && A_sex == 'M') || (B_age >= 19 && B_sex == 'M' ) )
+    {
+        cout << 1;
+    }
+
+    return 0;
+}
