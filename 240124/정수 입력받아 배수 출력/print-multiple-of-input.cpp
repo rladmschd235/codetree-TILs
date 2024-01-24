50b539bb0f038a3ee23598b6f3d7c211e8f0324f
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-24 22:29:11 +0900

    Title: 정수 입력받아 배수 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/정수 입력받아 배수 출력/print-multiple-of-input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    for(int i = n; i < (n * 5)+1; i+=n)
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
