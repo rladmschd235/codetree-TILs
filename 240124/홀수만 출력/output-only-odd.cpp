9664149a21c8505098ea3fb4e7fa600cbb920efb
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-24 22:30:18 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240124/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for(int i = a; i < b+1; i++)
+    {
+        if(i%2 == 1)
+        {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}
