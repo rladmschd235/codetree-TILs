a6376e5c39229eebf3c2be42101b63d947bd7831
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-25 20:40:02 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    while(a < b + 1)
+    {
+        cout << a << " ";
+        a += 2;
+    }
+    return 0;
+}
