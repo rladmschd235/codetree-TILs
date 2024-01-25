ca7ac6702d3b58d417c26548b8ff39e97e82483e
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-25 20:33:02 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240125/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, cnt = 1; 
+    cin >> n;
+    while(cnt < n+1)
+    {
+        cout << cnt << " ";
+        cnt++;
+    }
+    return 0;
+}
