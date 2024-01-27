e8c87e6f67614033680edbe6c886405caf5ba37e
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-27 21:40:10 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240127/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; 
+    cin >> a >> b;
+
+    if(a > b)
+    {
+        for(int i = a; i > b-1; i--)
+        {
+            cout << i << " ";
+        }
+    }
+    else
+    {
+        for(int i = b; i > a-1; i--)
+        {
+            cout << i << " ";
+        }
+    }
+    
+    return 0;
+}
