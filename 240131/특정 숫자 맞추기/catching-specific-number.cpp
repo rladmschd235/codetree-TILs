3e741616388988a50e83fcebda7fcf4ec0df51d1
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-01-31 23:11:51 +0900

    Title: 특정 숫자 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240131/특정 숫자 맞추기/catching-specific-number.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    while(true)
+    {
+        cin >> n;
+        if(n == 25)
+        {
+            cout << "Good" << endl;
+            break;
+        }
+        else if(n > 25)
+        {
+            cout << "Lower" << endl;
+        }
+        else if(n < 25)
+        {
+            cout << "Higher" << endl;
+        }
+    }
+    return 0;
+}
