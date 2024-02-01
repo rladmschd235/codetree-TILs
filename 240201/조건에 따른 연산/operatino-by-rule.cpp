c18820dc7dfa8246bf7f968014f87cababab73e9
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-01 15:33:17 +0900

    Title: 조건에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240201/조건에 따른 연산/operatino-by-rule.cpp
+++ 240201/조건에 따른 연산/operatino-by-rule.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 int main() {
-    int n, cnt = 1;
+    int n, cnt = 0;
     cin >> n;
     while(n < 1000)
     {
         cnt++;
         if(n%2 == 0)
         {
-            n *= 3 + 1;
+            n = (n*3) + 1;
         }
         else
         {
-            n *= 2 + 2;
+            n = (n*2) + 2;
         }
     }
     cout << cnt;
