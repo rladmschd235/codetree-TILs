cd97fb6258ace908f32f26fc540543e9e1564261
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-01 15:50:45 +0900

    Title: ab 사이에 없는 c, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240201/ab 사이에 없는 c/c-between-a-and-b-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    bool isChecked = false;
+    cin >> a >> b >> c;
+    for(int i = a; i < b+1; i++)
+    {
+        if(i%c == 0)
+        {
+            isChecked = true;
+        }
+    }
+    if(isChecked)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+    return 0;
+}
