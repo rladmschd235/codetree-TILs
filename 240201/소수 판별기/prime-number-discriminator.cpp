ca11439c7e1bb6b089398bbd4b9bd389892ffec8
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-01 15:53:32 +0900

    Title: 소수 판별기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240201/소수 판별기/prime-number-discriminator.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    bool isChecked = false;
+    for(int i = 2; i < n; i++)
+    {
+        if(n%i == 0)
+        {
+            isChecked = true;
+        }
+    }
+    if(!isChecked)
+    {
+        cout << 'P';
+    }
+    else
+    {
+        cout << 'C';
+    }
+    return 0;
+}
