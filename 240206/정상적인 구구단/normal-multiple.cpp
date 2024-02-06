7b9b6440d1e3dd277b3ab7289ee135f978e62d28
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-06 16:39:41 +0900

    Title: 정상적인 구구단, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240206/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 1; i < n+1; i++)
+    {
+        for(int j = 1; j < n+1; j++)
+        {
+            cout << i << " * " << j << " = " << i * j;
+            if(j != n)
+            {
+                cout << ", ";
+            }
+        }
+        cout << "\n";
+    }
+    return 0;
+}
