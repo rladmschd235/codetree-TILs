7ae07196eb6196a653a3fb8d88c59aedc06f2448
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-07 23:14:55 +0900

    Title: 특이한 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240207/특이한 출력/special-print.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+
+    for(int i = 1; i < n+1; i++)
+    {
+        for(int j = 1; j < n+1; j++)
+        {
+            cout << "(" << i << ", " << j << ")" << " ";
+            if((i+j) % 4 == 0)
+            {
+                cout << "\n";
+            }
+        }
+    }
+    return 0;
+}
