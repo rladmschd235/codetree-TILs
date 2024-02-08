3429a7967ec802c35cf4a1ddaf9a0e9cf09bb4da
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-08 19:11:52 +0900

    Title: 짝수의 합 n번 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/짝수의 합 n번 구하기/find-the-sum-of-even-numbers-n-times.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        int a, b, sum = 0;
+        cin >> a >> b;
+
+        for(int j = a; j < b+1; j++)
+        {
+            if(j%2 == 0)
+            {
+                sum += j;
+            }
+        }
+
+        cout << sum << endl;
+    }
+    return 0;
+}
