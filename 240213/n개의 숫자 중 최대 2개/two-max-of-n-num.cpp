28ec9e9d8cd898f79cd5e03b452b68727fbf0319
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-13 18:29:58 +0900

    Title: n개의 숫자 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
+++ 240213/n개의 숫자 중 최대 2개/two-max-of-n-num.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 int main() {
-    long long int arr[100] = {0};
+    int arr[100] = {0};
     int n;
 
     cin >> n;
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
     }
 
-    for(int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n-i; j++)
+        for (int j = 1; j < n - i; j++)
         {
-            if(arr[j] < arr[j+1])
+            if (arr[j] > arr[j - 1])
             {
                 int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }            
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+            }
         }
     }
 
