80363e3891a54ff97e0fe4795a03dc61f0a7f327
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-14 18:55:56 +0900

    Title: 문자 개수 세기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240214/문자 개수 세기/count-char.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    char c;
+    int cnt = 0;
+
+    getline(cin, str);
+    cin >> c;
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == c)
+        {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+
+    return 0;
+}
