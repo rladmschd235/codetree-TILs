2ee2d872ee2251f98d34525fdbcc70dc4f81e573
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-16 14:56:17 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240216/파스칼의 삼각형/pascal's-triangle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[15][15] = {1};
+    int n;
+
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i+1; j++)
+        {
+            if(j == i || j == 0)
+            {
+                arr[i][j] = 1;
+            }
+            else
+            {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < i+1; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
