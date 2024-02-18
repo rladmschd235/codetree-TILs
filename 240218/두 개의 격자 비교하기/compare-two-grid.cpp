e1ca5645ceec5044f770f8c9e490b887b7c7f9bc
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-18 15:30:52 +0900

    Title: 두 개의 격자 비교하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240218/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr1[10][10] = {0};
+    int arr2[10][10] = {0};
+    int n, m;
+
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin >> arr1[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin >> arr2[i][j];
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            if(arr1[i][j] == arr2[i][j])
+            {
+                cout << 0 << " ";
+            }
+            else
+            {
+                cout << 1 << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
