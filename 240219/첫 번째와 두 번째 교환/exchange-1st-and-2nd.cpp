459f12bccc5daff16e906dc8eb3de71aa30e005e
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-19 20:09:11 +0900

    Title: 첫 번째와 두 번째 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240219/첫 번째와 두 번째 교환/exchange-1st-and-2nd.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    char c1 = str[0];
+    char c2 = str[1];
+
+    for(int i = 0; i < str.length(); i++)
+    {
+        if(str[i] == c1)
+        {
+            str[i] = c2;
+            continue;
+        }
+        if(str[i] == c2)
+        {
+            str[i] = c1;
+            continue;
+        }
+    }
+
+    cout << str;
+
+    return 0;
+}
