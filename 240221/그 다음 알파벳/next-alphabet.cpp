2305ffd62713436504b01add3ccfd48e9a727e57
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-21 15:52:30 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240221/그 다음 알파벳/next-alphabet.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+
+    if(c == 'z')
+    {
+        cout << 'a';
+    }
+    else
+    {
+        cout << (char)(c+1);
+    }
+
+    return 0;
+}
