05ad79020e23d9ee19520bfd377364416b66d639
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-22 22:03:46 +0900

    Title: 정수만 추출하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/정수만 추출하기/extract-only-integers.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string A, B;
+    int sum = 0, idx;
+    cin >> A >> B;
+
+    idx = 0;
+
+    for(int i = 0; i < A.length(); i++)
+    {
+        if(A[i] >= '0' && A[i] <= '9')
+        {
+            continue;
+        }
+        else
+        {
+            idx = i;
+        }
+    }
+
+    sum += stoi(A.substr(0, idx+1));
+
+    idx = 0;
+
+    for(int i = 0; i < B.length(); i++)
+    {
+        if(B[i] >= '0' && B[i] <= '9')
+        {
+            continue;
+        }
+        else
+        {
+            idx = i;
+        }
+    }    
+
+    sum += stoi(B.substr(0, idx+1));
+
+    cout << sum;
+
+    return 0;
+}
