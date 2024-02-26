f4a8b2f94a49403312db11a7f24d8a323479b8ad
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-02-26 21:05:50 +0900

    Title: 함수를 이용한 369 게임, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240226/함수를 이용한 369 게임/369-games-using-functions.cpp
+++ 240226/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
+
 using namespace std;
 
-bool func(int n)
-{
-    if(n%3 == 0)
-    {
-        return true;
-    }
-    
-    while(n > 0)
-    {
-        int num = n%10;
-        
-        if(num%3 == 0 || num%6 == 0 || num%9 == 0)
-        {
+// 3, 6, 9 숫자가 
+// 단 하나라도 포함되었는지를 확인합니다.
+bool Contains369(int n) {
+    // 계속 10으로 나눠주며
+    // 일의 자리를 조사합니다.
+    while(n > 0) {
+        if(n % 10 == 3 || n % 10 == 6 || n % 10 == 9)
             return true;
-        }
-
         n /= 10;
     }
 
     return false;
 }
 
+// 3, 6, 9를 포함하거나 3의 배수인지를 판단합니다.
+bool Is369Number(int n) {
+    return Contains369(n) || (n % 3 == 0);
+}
+
 int main() {
-    int a, b, cnt = 0;
+    // 변수 선언 및 입력:
+    int a, b;
     cin >> a >> b;
-    
-    for(int i = a; i < b+1; i++)
-    {
-        if(func(i))
-        {
+
+    int cnt = 0;
+    for(int i = a; i <= b; i++)
+        if(Is369Number(i))
             cnt++;
-        }
-    }
 
     cout << cnt;
     return 0;
