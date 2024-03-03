f7b8aa4b989ee8c6cce63f4f673b7e774b8aa1e5
rladmschd <98925993+rladmschd235@users.noreply.github.com>
2024-03-03 20:38:38 +0900

    Title: 숫자 사각형을 출력하는 함수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240303/숫자 사각형을 출력하는 함수/function-outputs-numeric-square.cpp
+++ 240303/숫자 사각형을 출력하는 함수/function-outputs-numeric-square.cpp
@@ -2,19 +2,24 @@
 using namespace std;
 
 int main() {
-    int n;
+    int n, cnt = 1, arr[20][20] = {0};
     cin >> n;
 
     for(int i = 0; i < n; i++)
     {
-        int cnt = i+1;
-
         for(int j = 0; j < n; j++)
         {
-            cout << cnt << " ";
-            cnt += 4;
+            arr[j][i] = cnt;
+            cnt++;
         }
+    }
 
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
         cout << endl;
     }
     return 0;
